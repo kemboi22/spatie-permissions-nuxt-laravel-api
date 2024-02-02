5e77e12c547a58c7eeacdb3c10b0d67d4322503c
kemboi22 <kemboielvis22@gmail.com>
2024-02-02 22:13:03 +0300

    Initial Commit

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@pinia-plugin-persistedstate/nuxt",
+      "shadcn-nuxt"
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
